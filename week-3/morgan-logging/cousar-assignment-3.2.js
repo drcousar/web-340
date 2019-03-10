2b4c800e561879800618798a5d956a33a473a586
Donald Cousar <drcousar@yahoo.com>
2019-03-10 17:44:24 -0500

    Add Assignment 3.2

---
+++ week-3/morgan-logging/cousar-assignment-3.2.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+// Tell Express the views are in the 'views' directory
+app.set("views", path.resolve(__dirname, "views")); 
+app.set("view engine", "ejs"); // Tell Express to use the EJS view engine
+app.use(logger("short"));
+app.get("/", function (request, response) {
+    response.render("index", {
+        message: "Welcome to the Morgan Logger Example!"
+    });
+});
+
+http.createServer(app).listen(8080, function() {
+    console.log("Application started on port 8080");
+});
