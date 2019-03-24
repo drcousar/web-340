78ff78212c274714f863a499bae43fdb34d34575
Donald Cousar <drcousar@yahoo.com>
2019-03-24 07:42:30 -0500

    Add assignment 5.2

---
+++ week-5/if-else-render/cousar-assignment-5.2.js
@@ -0,0 +1,45 @@
+/*
+============================================
+; Title:  cousar-assignment-5.3.js
+; Author: Don Cousar
+; Modified By: Don Cousar
+; Date:   24 March 2019
+; Description: ejs
+;===========================================
+*/
+// start program
+
+//Call Header Info
+var header = require('../../cousar-header');
+console.log(header.display("Donald", "Cousar","Exersize 5.3"));
+console.log('\n');
+
+var express = require("express");
+var http = require("http");
+var path = require("path");
+
+app = express();
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//Create list of names
+var n = [
+  "John",
+  "Susan",
+  "Don",
+  "Sandy"
+];
+
+//pass names to ejs
+app.get("/", function(request, response) {
+    response.render("index", {
+        names: n
+    })
+});
+
+//start http server
+http.createServer(app).listen(8080, function() {
+    console.log("Application started on port 8080!");
+});
+
+//End Program
