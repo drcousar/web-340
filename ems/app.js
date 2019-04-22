e11821072d616220d4b2be2b1dbefc69f7b775d9
Donald Cousar <drcousar@yahoo.com>
2019-04-22 00:02:02 -0500

    Assignment 9.3

--- ems/app.js
+++ ems/app.js
@@ -144,6 +144,7 @@ app.post("/process", function (req, res) {
 })
 
 //creating node web server
-http.createServer(app).listen(8080, function () {
-    console.log("Application started on port 8080!")
+app.set("port", process.env.PORT || 8080)
+http.createServer(app).listen(app.get("port"), function () {
+    console.log("Application started on port " + app.get("port"))
 });
